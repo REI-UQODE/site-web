cb06d2aa949f6a5d3a394b03560652192a7de1dc
a <a@a>
2026-03-17 13:29:33 -0400

    Ajout automatique de la feuille css

--- site/ajouterCommun.js
+++ site/ajouterCommun.js
@@ -1,4 +1,12 @@
 export async function ajouterBasDePage(){
+    // Ajouter la feuille css
+    let head = document.getElementsByTagNameNS("head")[0];
+    let css = document.createElement("link");
+    css.rel = "stylesheet";
+    css.type = "text/css";
+    css.href = "/footer.css";
+    head.appendChild(css);
+
     let balise = document.getElementsByTagName("footer")[0];
     let html = "";
     await fetch("basDePage.html").then(r => r.text()).then(r => html=r);
